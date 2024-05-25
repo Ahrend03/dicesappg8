93972b7185dfa2732738e6c21079b7b6ff9b564f
Ahrend <caguilar@agrokasa.com.pe>
2024-05-25 10:48:28 -0500

    modificacion 3 en main

--- lib/pages/dice_view.dart
+++ lib/pages/dice_view.dart
@@ -1,6 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget{
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePage();
+}
+
+
+class _DicePage extends State<DicePage>{
+  int n =3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +33,44 @@ class DicePage extends StatelessWidget{
               padding: EdgeInsets.all(16),
               width: 200,
               height: 200,
-              child: Image.asset("assets/images/dice1.png"),
+              child: Image.asset("assets/images/dice$n.png"),
               ),
+              SizedBox(height: 24,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed:(){
+                      if(n==1){
+                        n=6;
+                      }else{
+                       n--;
+                      }
+                      setState(() {});
+                    },
+                   child: Icon(Icons.arrow_back_ios),
+                   ),
+                   ElevatedButton(
+                    onPressed:(){
+                       if(n==6){
+                        n=1;
+                      }else{
+                       n++;
+                      }
+                      setState(() {});
+                    },
+                    child: Icon(Icons.arrow_forward_ios))
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              ElevatedButton(
+              onPressed: (){
+                n = Random().nextInt(6)+1;
+                setState(() {});
+              },
+               child: Text("Random"))
           ],
           ),
         ),
